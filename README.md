afec70367a5a6c76e0906e0b2199163899f7ac6e
chauid <dlfdl28@naver.com>
2025-04-23 13:48:16 +0900

    add usage

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ## jenkins-script
 jenkins shared library
+
+
+### Usage
+|function|return|params|
+|---|:---:|:---|
+|`build`|void|void|
+|`build.gradle`|void|void|
+|`build.docker`|void|String imageName, String imageTag, boolean useKaniko|
+|`k8s`|void|void|
+|`k8s.deploy`|void|String deploymentName, String namespace, String imageName, String imageTag, boolean initDeploy|
--- vars/bulid.groovy
+++ vars/bulid.groovy
@@ -9,7 +9,10 @@ def gradle() {
     buildUtils.gradleBuild("${env.WORKSPACE}")
 }
 
-def docker(String imageName, String imageTag, boolean useKaniko = false) {
+def docker(String imageName = 'none', String imageTag = 'none', boolean useKaniko = false) {
+    if (imageName == 'none' || imageTag == 'none') {
+        error 'Image name and tag must be provided.'
+    }
     def buildUtils = new BuildUtils(steps)
     buildUtils.gradleBuild("${env.WORKSPACE}", imageName, imageTag, useKaniko)
 }
